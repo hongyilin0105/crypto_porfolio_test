dd088f4e54e7b82a6a1f9ce551ce008d06522018
Hongyi Lin <hongyi.lin@wustl.edu>
2017-12-13 21:44:18 -0600

    Added more methods

--- test_api.py
+++ test_api.py
@@ -9,20 +9,47 @@ client = Client(API_KEY, API_SECRET)
 user = client.get_current_user()
 user_as_json_string = json.dumps(user)
 
-def get_account_balance(client, ifUSD = True):
+def json_account_to_dict(client):
     all_accounts_dict = json.loads(json.dumps(client.get_accounts()))
     cleaned_accounts_json_string = json.dumps(all_accounts_dict['data'])
     cleaned_accounts_dict = json.loads(cleaned_accounts_json_string)
-    num_of_accounts = len(cleaned_accounts_dict)
+    return cleaned_accounts_dict
+
+#Grab the account ID for: ALL(dict), BTC, ETH or LTC(id string)
+def get_account_id(client, currency = "ALL"):
+    if currency not in ["ALL", "BTC", "ETH", "LTC"]:
+        return "you suck"
+    wallet_name = currency + str(" Wallet")
+    accounts_dict = json_account_to_dict(client)
+    num_of_accounts = len(accounts_dict)
+    res = {}
+    for a in range(num_of_accounts):
+        res.update({accounts_dict[a].get('name')
+                    : accounts_dict[a].get('id')})
+    if currency == "ALL":
+        return res
+    else:
+        return res[wallet_name]
+
+#For US-native accounts, grab the current account balance for: BTC, ETH or LTC(float)
+#Default: USD value for all currencies
+def get_account_balance(client, currency = "ALL", ifUSD = True):
+    accounts_dict = json_account_to_dict(client)
+    num_of_accounts = len(accounts_dict)
     res = {}
     if ifUSD:
         for a in range(num_of_accounts):
-            res.update({cleaned_accounts_dict[a].get('balance').get('currency')+str(' in USD')
-                        : cleaned_accounts_dict[a].get('native_balance').get('amount')})
+            res.update({accounts_dict[a].get('balance').get('currency')+str(' in USD')
+                        : accounts_dict[a].get('native_balance').get('amount')})
+        if currency in ["BTC", "ETH", "LTC"]:
+            res = res[currency+str(' in USD')]
     else:
         for a in range(num_of_accounts):
-            res.update({cleaned_accounts_dict[a].get('balance').get('currency')
-                       : cleaned_accounts_dict[a].get('balance').get('amount')})
+            res.update({accounts_dict[a].get('balance').get('currency')
+                       : accounts_dict[a].get('balance').get('amount')})
+        if currency in ["BTC", "ETH", "LTC"]:
+            res = res[currency]
     return res
+
 
 get_account_balance(client, False)
