98aa383b9205c4c464e94f109f765124aa51806d
Hongyi Lin <hongyi.lin@wustl.edu>
2017-12-30 12:50:41 -0600

    Fix printing trading history

--- binance_analysis.py
+++ binance_analysis.py
@@ -1,14 +1,15 @@
 #import binance
 from binance.client import Client
+import string
 
 api_file = open('binance_api_key.txt', 'r')
 API_KEY = api_file.readline()[:-1]  # your API key
 API_SECRET = api_file.readline()[:-1]  # your API secret
 client = Client(API_KEY, API_SECRET)
 
-interested_pairs = open('interested_pairs.txt', 'r')
-
-while interested_pairs:
-    pair = interested_pairs.readline()[:-1]
-    print str(pair)+":"
-    print client.get_historical_trades(symbol=pair)
+#print all trade history for trading pairs of interest
+with open('interested_pairs.txt', 'r') as pairs:
+    for pair in pairs:
+        symbol = string.replace(pair, '\n','')
+        print symbol +":"
+        print client.get_historical_trades(symbol=symbol)
